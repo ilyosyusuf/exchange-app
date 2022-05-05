2461fe28b2b7fcc0f51f26ee6f0246c4c23ec5c1
ilyosyusuf <ilyosyusufforweb@gmail.com>
2022-05-05 10:47:33 +0500

    added hive box

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:oneapp/models/currency_model.dart';
 import 'package:oneapp/screens/homepage.dart';
+import 'package:oneapp/services/currency_service.dart';
 
 void main() async {
   await Hive.initFlutter();
+  await CurrencyService.openBox();
+  Hive.registerAdapter(CurrencyModelAdapter());
   runApp(const MyApp());
 }
 
---
+++ lib/services/currency_service.dart
@@ -0,0 +1,35 @@
+
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:oneapp/models/currency_model.dart';
+import 'package:path_provider/path_provider.dart';
+
+class CurrencyService{
+  static Box<CurrencyModel>? box;
+
+  static Future<List<CurrencyModel>> getCurrencies()async{
+    Response res = await Dio().get('https://nbu.uz/en/exchange-rates/json/');
+    List<CurrencyModel> d = (res.data as List).map((e) => CurrencyModel.fromJson(e)).toList();
+    await putData(d);
+
+    return d;
+  }
+
+
+  static openBox()async{
+    box = await Hive.openBox('currencybox');
+    
+    Directory appDocDir = await getApplicationDocumentsDirectory();
+    Hive.init(appDocDir.path);
+  }
+
+  static putData(List<CurrencyModel> data)async{
+    for(var item in data){
+      box!.add(item);
+    }
+  }
+
+}
